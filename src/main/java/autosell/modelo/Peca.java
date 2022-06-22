83ee3f1b85f3fdf9a6591023ca1b306d3925c557
Ricardo Franco <ricardosf195@gmail.com>
2022-06-22 15:57:21 +0100

    Classe Peca implementada

--- src/main/java/autosell/modelo/Peca.java
+++ src/main/java/autosell/modelo/Peca.java
@@ -1,5 +1,13 @@
 package autosell.modelo;
 
 public class Peca {
+    private String nome;
 
+    public Peca(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
 }
