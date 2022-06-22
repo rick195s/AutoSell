7b023eff81d91dcf9e116e7a101806622e2a6438
Ricardo Franco <ricardosf195@gmail.com>
2022-06-22 18:03:40 +0100

    Testes unitarios ao adicionar transacao implementados

--- src/test/java/AddTransacaoTeste.java
+++ src/test/java/AddTransacaoTeste.java
@@ -27,6 +27,47 @@ public class AddTransacaoTeste {
 
         assertEquals( Data.parseData("12/03/2002").toString(), DadosDaAplicacao.INSTANCE.getTransacoes().getFirst().getData().toString());
 
+    }
+
+    @Test
+    public void TesteTransacaoAdicionada(){
+        Cliente cliente = new Cliente("Ricardo", 21321311);
+        Veiculo veiculo = new Veiculo("M1", "AD-Dw-12", "BMW");
+        Local local = DadosDaAplicacao.INSTANCE.getLocais().getFirst();
+
+        Transacao transacao1 = new Transacao(
+                Data.parseData("12/03/2001"),
+                cliente,
+                veiculo,
+                local);
+
+        DadosDaAplicacao.INSTANCE.addTransacao(transacao1);
+
+        assertNotEquals(-1, DadosDaAplicacao.INSTANCE.getTransacoes().indexOf(transacao1));
 
     }
+
+    @Test
+    public void TesteInformacaoTransacao(){
+        Cliente cliente = new Cliente("Ricardo", 21321311);
+        Veiculo veiculo = new Veiculo("M1", "AD-Dw-12", "BMW");
+        Local local = DadosDaAplicacao.INSTANCE.getLocais().getFirst();
+
+        Transacao transacao1 = new Transacao(
+                Data.parseData("12/03/2001"),
+                cliente,
+                veiculo,
+                local);
+
+        DadosDaAplicacao.INSTANCE.addTransacao(transacao1);
+
+        Transacao transacaoObtida = DadosDaAplicacao.INSTANCE.getTransacoes().getFirst();
+        assertEquals(0, transacaoObtida.getData().compareTo(Data.parseData("12/03/2001")) );
+        assertEquals( cliente,transacaoObtida.getCliente());
+        assertEquals(local,transacaoObtida.getLocal());
+        assertEquals( veiculo, transacaoObtida.getVeiculo());
+
+    }
+
+
 }
