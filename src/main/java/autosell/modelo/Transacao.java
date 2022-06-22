eb0c6cba91cf3e439168e680edb24ca701779b6b
Ricardo Franco <ricardosf195@gmail.com>
2022-06-22 17:23:38 +0100

    alteracao para a Transacao usar a classe Data

--- src/main/java/autosell/modelo/Transacao.java
+++ src/main/java/autosell/modelo/Transacao.java
@@ -1,28 +1,20 @@
 package autosell.modelo;
 
-import java.util.Date;
-
 public class Transacao {
-    private Date data;
-    private String tipo;
+    private Data data;
     private Cliente cliente;
     private Veiculo veiculo;
     private Local local;
 
-    public Transacao(Date data, String tipo, Cliente cliente, Veiculo veiculo, Local local) {
+    public Transacao(Data data, String tipo, Cliente cliente, Veiculo veiculo, Local local) {
         this.data = data;
-        this.tipo = tipo;
         this.cliente = cliente;
         this.veiculo = veiculo;
         this.local = local;
     }
 
-    public Date getData() {
+    public Data getData() {
         return data;
-    }
-
-    public String getTipo() {
-        return tipo;
     }
 
     public Cliente getCliente() {
