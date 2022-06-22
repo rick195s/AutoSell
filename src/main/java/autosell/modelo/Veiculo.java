a5ca7f50095ca1b8ce5740df37faf000c30efb4e
Ricardo Franco <ricardosf195@gmail.com>
2022-06-22 15:43:38 +0100

    Classe Veiculo implementada

--- src/main/java/autosell/modelo/Veiculo.java
+++ src/main/java/autosell/modelo/Veiculo.java
@@ -1,4 +1,26 @@
 package autosell.modelo;
 
 public class Veiculo {
+
+    private String nome;
+    private String matricula;
+    private String marca;
+
+    public Veiculo(String nome, String matricula, String marca) {
+        this.nome = nome;
+        this.matricula = matricula;
+        this.marca = marca;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
 }
