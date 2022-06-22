66880c651562319f3ea4d596f39d6114cb494e1c
Ricardo Franco <ricardosf195@gmail.com>
2022-06-22 17:29:33 +0100

    alteracao da classe Data

--- src/main/java/autosell/modelo/Data.java
+++ src/main/java/autosell/modelo/Data.java
@@ -29,10 +29,13 @@ public class Data {
 
     @Override
     public String toString() {
-
         return String.format("%02d/%02d/%04d",
                 calendar.get(Calendar.DAY_OF_MONTH),
                 calendar.get(Calendar.MONTH) + 1,
                 calendar.get(Calendar.YEAR));
+    }
+
+    public int compareTo(Data o1){
+        return calendar.compareTo(o1.calendar);
     }
 }
