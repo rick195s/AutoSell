913b1941e8daa2212f07114366b4cd5808fd830d
Ricardo Franco <ricardosf195@gmail.com>
2022-06-22 15:41:47 +0100

    Classe Cliente implementada

--- src/main/java/autosell/modelo/Cliente.java
+++ src/main/java/autosell/modelo/Cliente.java
@@ -1,4 +1,19 @@
 package autosell.modelo;
 
 public class Cliente {
+    private String nome;
+    private double NIF;
+
+    public Cliente(String nome, double NIF) {
+        this.nome = nome;
+        this.NIF = NIF;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getNIF() {
+        return NIF;
+    }
 }
--- src/main/java/autosell/modelo/DadosDaAplicacao.java
+++ src/main/java/autosell/modelo/DadosDaAplicacao.java
@@ -1,4 +1,8 @@
 package autosell.modelo;
 
 public class DadosDaAplicacao {
+    public static DadosDaAplicacao INSTANCE = new DadosDaAplicacao();
+
+    private DadosDaAplicacao() {
+    }
 }
