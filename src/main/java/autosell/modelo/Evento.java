55ae9867d297174cdd9a6746bc77301b7b8037a1
Ricardo Franco <ricardosf195@gmail.com>
2022-06-22 15:56:33 +0100

    Classe Evento implementada

--- src/main/java/autosell/modelo/Evento.java
+++ src/main/java/autosell/modelo/Evento.java
@@ -1,4 +1,12 @@
 package autosell.modelo;
 
 public class Evento {
+
+    private Local local;
+    private String nome;
+
+    public Evento(Local local, String nome) {
+        this.local = local;
+        this.nome = nome;
+    }
 }
